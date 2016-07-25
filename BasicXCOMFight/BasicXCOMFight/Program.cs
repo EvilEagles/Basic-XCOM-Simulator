c67e703312529d6b08cb55bbabda8e47e5a51d4c
Alex Vu <alex@igv.cafe>
2016-07-25 22:16:29 +0700

    Added Functions

--- BasicXCOMFight/BasicXCOMFight/Program.cs
+++ BasicXCOMFight/BasicXCOMFight/Program.cs
@@ -8,15 +8,6 @@ namespace BasicXCOMFight
 {
     class Program : Units
     {
-        // UI: USER COMMANDS
-        public void showCommand(int hit_chance, int crit)
-        {
-            Console.WriteLine("1. Take a Shot - Hit Chance: {0}% | Crit Chance: {1}%",
-                              hit_chance, crit);
-            Console.WriteLine("2. Hunker Down");
-            Console.WriteLine("3. Move Up - Costs 1 Action");
-            Console.WriteLine("|==========================|");
-        }
         static void Main(string[] args)
         {
             Units player = new Units();         // Creating player's character
@@ -37,6 +28,7 @@ namespace BasicXCOMFight
             // RNG SYSTEM
             Random rnd = new Random();
             int dice;
+            int act_chance;
             int damage;
             int distance = rnd.Next(10, 18);
             #endregion
@@ -66,30 +58,33 @@ namespace BasicXCOMFight
                 Console.WriteLine("        XCOM ACTIVITY       ");
                 Console.WriteLine("| Turn: {0} | Distance: {1}", turn, distance);
                 Console.WriteLine("|==========================|");
+                Console.WriteLine();
                 Console.WriteLine("Name: {0}", player.name);
                 Console.WriteLine("HP: {0}/{1}", player.hp, player.maxHP);
                 Console.WriteLine("Aim: {0}", player.aim);
                 Console.WriteLine("Defense: {0}", player.def);
                 if (player.cover == half_cover) Console.WriteLine("Cover: Half Cover (+{0} Defense)", half_cover);
                 else Console.WriteLine("Cover: Full Cover (+{0} Defense)", full_cover);
+                Console.WriteLine();
                 Console.WriteLine("|=========== VS ===========|");
+                Console.WriteLine();
                 Console.WriteLine("Name: {0}", enemy.name);
                 Console.WriteLine("HP: {0}/{1}", enemy.hp, enemy.maxHP);
                 Console.WriteLine("Aim: {0}", enemy.aim);
                 Console.WriteLine("Defense: {0}", enemy.def);
                 if (enemy.cover == half_cover) Console.WriteLine("Cover: Half Cover (+{0} Defense)", half_cover);
                 else Console.WriteLine("Cover: Full Cover (+{0} Defense)", full_cover);
-                Console.WriteLine("|==========================|");
-                #endregion
-                // PRECONDITIONS
-                #region
-                if (distance >= 7) hit_chance = player.aim - enemy.def - enemy.cover + ((18 - distance) * 2);
-                else hit_chance = player.aim - enemy.def - enemy.cover + 22 + ((7 - distance) * 4);
                 #endregion
 
                 while (loop == true)
                 {
-                    Program ui = new Program();
+                    // PRECONDITIONS
+                    #region
+                    if (distance >= 7) hit_chance = player.aim - enemy.def - enemy.cover + ((18 - distance) * 2);
+                    else hit_chance = player.aim - enemy.def - enemy.cover + 22 + ((7 - distance) * 4);
+                    #endregion
+
+                    UIAndActions ui = new UIAndActions();
                     ui.showCommand(hit_chance, player.crit);
 
                     // INPUT COMMANDS
@@ -112,9 +107,7 @@ namespace BasicXCOMFight
                         }
                         else                        // IF: Shot misses
                         {
-                            System.Threading.Thread.Sleep(1000);
-                            Console.WriteLine();
-                            Console.WriteLine("{0} took a shot at {1}. It is a miss.", player.name, enemy.name);
+                            ui.shotMissed(player.name, enemy.name);
                             break;
                         }
                     }
@@ -132,17 +125,13 @@ namespace BasicXCOMFight
                         if (alreadyMoved == false)
                         {
                             distance--;
-                            System.Threading.Thread.Sleep(1000);
-                            Console.WriteLine();
-                            Console.WriteLine("{0} closed in. Distance decreased by 1.", player.name);
+                            ui.moveUp(player.name);
                             alreadyMoved = true;
                         }
                         else
                         {
                             distance--;
-                            System.Threading.Thread.Sleep(1000);
-                            Console.WriteLine();
-                            Console.WriteLine("{0} closed in. Distance decreased by 1.", player.name);
+                            ui.moveUp(player.name);
                             break;
                         }
                     }
@@ -162,50 +151,73 @@ namespace BasicXCOMFight
                     enemy.cover /= 2;
                     e_hunker = false;
                 }
-                // Calculating Hit Chance influenced by Distance
-                if (distance >= 7) hit_chance = enemy.aim - player.def - player.cover + ((18 - distance) * 2);
-                else hit_chance = enemy.aim - player.def - player.cover + 22 + ((7 - distance) * 4);
-
-                if (hit_chance >= 45)   // AI: If hit chance is higher than X% (1)
+                alreadyMoved = false;
+                while (loop == true)
                 {
-                    dice = rnd.Next(1, 100);
-                    if (dice <= 80)     // AI: (1) Roll a dice to determine decision. If yes, take a shot (2)
+                    // Calculating Hit Chance influenced by Distance
+                    if (distance >= 7) hit_chance = enemy.aim - player.def - player.cover + ((18 - distance) * 2);
+                    else hit_chance = enemy.aim - player.def - player.cover + 22 + ((7 - distance) * 4);
+
+                    if (hit_chance >= 40)   // AI: If hit chance is higher than X% (1)
                     {
+                        act_chance = 70;
                         dice = rnd.Next(1, 100);
-                        if (dice <= hit_chance)     //IF: Shot hits
+                        if (dice <= act_chance)     // AI: (1) Roll a dice to determine decision. If yes, take a shot (2)
                         {
-                            damage = rnd.Next(1, 3);
-                            System.Threading.Thread.Sleep(1000);
-                            Console.WriteLine();
-                            Console.WriteLine("{1} took an accurate shot and {0} took {2} damage.",
-                                                                  player.name, enemy.name, damage);
-                            player.hp -= damage;
+                            dice = rnd.Next(1, 100);
+                            if (dice <= hit_chance)     //IF: Shot hits
+                            {
+                                damage = rnd.Next(1, 3);
+                                System.Threading.Thread.Sleep(1000);
+                                Console.WriteLine();
+                                Console.WriteLine("ALIEN ACTIVITY! {1} took an accurate shot and {0} took {2} damage.",
+                                                                      player.name, enemy.name, damage);
+                                player.hp -= damage;
+                                break;
+                            }
+                            else                        //IF: Shot misses
+                            {
+                                System.Threading.Thread.Sleep(1000);
+                                Console.WriteLine();
+                                Console.WriteLine("ALIEN ACTIVITY! {1} took a shot at {0}. It is a miss.", player.name, enemy.name);
+                                break;
+                            }
                         }
-                        else                        //IF: Shot misses
+                        else                            // IF (2) is false, then ... (3)
                         {
-                            System.Threading.Thread.Sleep(1000);
-                            Console.WriteLine();
-                            Console.WriteLine("{1} took a shot at {0}. It is a miss.", player.name, enemy.name);
+                            act_chance = 50;
+                            dice = rnd.Next(1, 100);
+                            if (dice <= act_chance)     // (3) Hunker Down
+                            {
+                                System.Threading.Thread.Sleep(1000);    
+                                Console.WriteLine();
+                                Console.WriteLine("ALIEN ACTIVITY! {0} hunkered down, doubling cover bonus.", enemy.name);
+                                enemy.cover *= 2;
+                                e_hunker = true;
+                                break;
+                            }
+                            else
+                            {
+                                distance--;
+                                System.Threading.Thread.Sleep(1000);
+                                Console.WriteLine();
+                                Console.WriteLine("{0} closed in. Distance decreased by 1.", player.name);
+                                alreadyMoved = true;
+                            }
                         }
                     }
-                    else                            // IF (2) is false, then Hunker Down
+                    else                                // If (1) is false, then Hunker down
                     {
                         System.Threading.Thread.Sleep(1000);
                         Console.WriteLine();
-                        Console.WriteLine("{0} hunkered down, doubling cover bonus.", enemy.name);
+                        Console.WriteLine("ALIEN ACTIVITY! {0} hunkered down, doubling cover bonus.", enemy.name);
                         enemy.cover *= 2;
                         e_hunker = true;
+                        break;
                     }
-                }
-                else                                // If (1) is false, then Hunker down
-                {
-                    System.Threading.Thread.Sleep(1000);
-                    Console.WriteLine();
-                    Console.WriteLine("{0} hunkered down, doubling cover bonus.", enemy.name);
-                    enemy.cover *= 2;
-                    e_hunker = true;
-                }
+                }   // End of Loop: Alien Activity
             }   // End of Loop: Turn
-        }       // End of Method: Main
-    }           // End of Class: Program
-}               // End of Namespace
+        }   // End of Method: Main
+    }    // End of Class: Program
+}    // End of Namespace  
+
---
+++ BasicXCOMFight/BasicXCOMFight/UIAndActions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicXCOMFight
+{
+    class UIAndActions
+    {
+        // UI: USER COMMANDS
+        public void showCommand(int hit_chance, int crit)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("|========= COMMAND ========|");
+            Console.WriteLine("1. Take a Shot - Hit Chance: {0}% | Crit Chance: {1}%",
+                              hit_chance, crit);
+            Console.WriteLine("2. Hunker Down");
+            Console.WriteLine("3. Move Up - Costs 1 Action");
+            Console.WriteLine("|==========================|");
+        }
+        // ACTION: TAKING SHOT
+        public void takeShot(string user_name, string target_name, int damage)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("{0} took an accurate shot and {1} took {2} damage.", user_name, target_name, damage);
+        }
+        // ACTION: MISSING SHOT
+        public void shotMissed(string user_name, string target_name)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("{0} took a shot at {1}. It is a miss.", user_name, target_name);
+        }
+        // ACTION: MOVING UP
+        public void moveUp(string user_name)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine();
+            Console.WriteLine("{0} closed in. Distance decreased by 1.", user_name);
+        }
+
+    }   // END of Class UIAndActions
+}   // END of namespace BasicXCOMFight
